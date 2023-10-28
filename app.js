28f90615e6583323fbd49fd2cc788a98a7120fbe
José Cardoso <115185989+cardosojse@users.noreply.github.com>
2023-10-28 14:30:14 -0300

    modified

--- app.js
+++ app.js
@@ -12,3 +12,8 @@ askBtn.addEventListener("click", () => {
     let answer = replys[Math.floor(Math.random() * replys.length)];
     answerBox.innerText = `O gato escolheu: ${answer}.`;
 });
+
+if(navigator.userAgent.indexOf('iPhone') > -1 ){
+    document.querySelector("[name=viewport]")
+     .setAttribute("content","width=device-width, initial-scale=1, maximum-scale=1");
+}
